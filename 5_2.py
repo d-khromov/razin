3815b25a0500d386849715e4fa92556310624d61
d-khromov <khromov.da@phystech.edu>
2020-10-02 10:51:19 +0300

    Add animation

--- 5_2.py
+++ 5_2.py
@@ -2,11 +2,9 @@ import pygame
 from pygame.draw import *
 import numpy as np
 
-
 clock = pygame.time.Clock()
 pygame.init()
 FPS = 30
-
 
 # цвета
 purple = (140, 78, 148)
@@ -18,7 +16,7 @@ pink = (255, 199, 199)
 dark_purple = (28, 0, 33)
 
 # размеры
-SCALE = 0.5
+SCALE = 0.3
 SCREEN_WIDTH = round(2500 * SCALE)
 SCREEN_HEIGHT = round(1667 * SCALE)
 SUN_RADIUS = round(140 * SCALE)
@@ -43,7 +41,8 @@ MOUNTAINS_1_COORDINATES = [[0, 776], [0, 678], [514, 330], [608, 357], [652, 419
                            [1127, 603], [1211, 648], [1335, 529], [
                                1440, 554], [1501, 500], [1819, 264],
                            [1869, 305], [1982, 411], [2072, 392], [2242, 476], [2325, 430], [2500, 517], [0, 776]]
-MOUNTAINS_2_COORDINATES = [[0, 832], [64, 864], [221, 669], [433, 1066], [545, 879], [721, 974], [800, 752], [1021, 800],
+MOUNTAINS_2_COORDINATES = [[0, 832], [64, 864], [221, 669], [433, 1066], [545, 879], [721, 974], [800, 752],
+                           [1021, 800],
                            [1200, 932], [1433, 884], [1712, 680], [2041, 883], [
                                2152, 750], [2254, 821], [2304, 740],
                            [2402, 750], [2500, 590], [2500, 1074], [0, 1129]]
@@ -63,12 +62,16 @@ for point in MOUNTAINS_3_COORDINATES:
     point[0] = round(point[0] * SCALE)
     point[1] = round(point[1] * SCALE)
 
+
 # функция, рисующая птицу
-def draw_bird(screen, color, x, y, size):
-    BIRD_COORDINATES = [[x, y], [x + round(81*size), y - round(46*size)], [x + round(29*size), y - round(39*size)],
-                        [x + round(7*size), y - round(23*size)], [x -
-                                                                  round(48*size), y - round(44*size)],
-                        [x - round(72*size), y - round(41*size)], [x, y]]
+def draw_bird(screen, color, x, y, size, time):
+    BIRD_COORDINATES = [[x, y],
+                        [x + round(81 * size), (y - round(46 * size)) * (1 + np.sin(time) / 50)],
+                        [x + round(29 * size), (y - round(39 * size)) * (1 + np.sin(time) / 50)],
+                        [x + round(7 * size), y - round(23 * size)],
+                        [x - round(48 * size), (y - round(44 * size)) * (1 + np.sin(time) / 50)],
+                        [x - round(72 * size), (y - round(41 * size)) * (1 + np.sin(time) / 50)],
+                        [x, y]]
     for point in BIRD_COORDINATES:
         point[0] = round(point[0] * SCALE)
         point[1] = round(point[1] * SCALE)
@@ -78,34 +81,45 @@ def draw_bird(screen, color, x, y, size):
 # экран
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-# задний фон
-rect(screen, BACKGROUND_1_COLOR, (0, 0, SCREEN_WIDTH, BACKGROUND_2_POSITION - 0))
-rect(screen, BACKGROUND_2_COLOR, (0, BACKGROUND_2_POSITION,
-                                  SCREEN_WIDTH, BACKGROUND_3_POSITION - BACKGROUND_2_POSITION))
-rect(screen, BACKGROUND_3_COLOR, (0, BACKGROUND_3_POSITION,
-                                  SCREEN_WIDTH, BACKGROUND_4_POSITION - BACKGROUND_3_POSITION))
-rect(screen, BACKGROUND_4_COLOR, (0, BACKGROUND_4_POSITION,
-                                  SCREEN_WIDTH, SCREEN_HEIGHT - BACKGROUND_2_POSITION))
-
-# горы
-polygon(screen, MOUNTAINS_1_COLOR, MOUNTAINS_1_COORDINATES)
-polygon(screen, MOUNTAINS_2_COLOR, MOUNTAINS_2_COORDINATES)
-polygon(screen, MOUNTAINS_3_COLOR, MOUNTAINS_3_COORDINATES)
-
-# Солнце
-circle(screen, SUN_COLOR, SUN_POSITION, SUN_RADIUS)
-
-# птицы
-for bird in BIRDS_COORDINATES_AND_SIZES:
-    draw_bird(screen, BIRD_COLOR, bird[0], bird[1], bird[2])
-    
-
-    
 pygame.display.update()
 finished = False
+time = 0
+
+alpha = np.random.random(len(BIRDS_COORDINATES_AND_SIZES))
+
 while not finished:
+    time += 0.1
     clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
+    # задний фон
+    rect(screen, BACKGROUND_1_COLOR, (0, 0, SCREEN_WIDTH, BACKGROUND_2_POSITION - 0))
+    rect(screen, BACKGROUND_2_COLOR, (0, BACKGROUND_2_POSITION,
+                                      SCREEN_WIDTH, BACKGROUND_3_POSITION - BACKGROUND_2_POSITION))
+    rect(screen, BACKGROUND_3_COLOR, (0, BACKGROUND_3_POSITION,
+                                      SCREEN_WIDTH, BACKGROUND_4_POSITION - BACKGROUND_3_POSITION))
+    rect(screen, BACKGROUND_4_COLOR, (0, BACKGROUND_4_POSITION,
+                                      SCREEN_WIDTH, SCREEN_HEIGHT - BACKGROUND_2_POSITION))
+
+    # горы
+    polygon(screen, MOUNTAINS_1_COLOR, MOUNTAINS_1_COORDINATES)
+    polygon(screen, MOUNTAINS_2_COLOR, MOUNTAINS_2_COORDINATES)
+    polygon(screen, MOUNTAINS_3_COLOR, MOUNTAINS_3_COORDINATES)
+
+    # Солнце
+    circle(screen, SUN_COLOR, SUN_POSITION, SUN_RADIUS)
+
+    # птицы
+    i = 0
+    for bird in BIRDS_COORDINATES_AND_SIZES:
+        new_x = bird[0] * 1
+        new_y = bird[1] * (1 + np.cos(alpha[i] * time) / 20)
+        new_size = bird[2] * (1 + time ** 0.5 / 10)
+        draw_bird(screen, BIRD_COLOR, new_x, new_y, new_size, time)
+
+        i += 1
+
+    pygame.display.update()
+
 pygame.quit()
